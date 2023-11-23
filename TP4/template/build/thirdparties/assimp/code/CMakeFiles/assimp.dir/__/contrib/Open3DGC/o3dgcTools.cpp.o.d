6121885f7885dc3e2358a25170bac9f33c13a409
e20190010736 <louis.jean34@hotmail.fr>
2023-11-23 18:11:27 +0100

    Début du TP4 sur les textures

---
+++ TP4/template/build/thirdparties/assimp/code/CMakeFiles/assimp.dir/__/contrib/Open3DGC/o3dgcTools.cpp.o.d
@@ -0,0 +1,3 @@
+thirdparties/assimp/code/CMakeFiles/assimp.dir/__/contrib/Open3DGC/o3dgcTools.cpp.o: \
+ /home/e20190010736/prog-3D-2023/template/thirdparties/assimp/contrib/Open3DGC/o3dgcTools.cpp \
+ /usr/include/stdc-predef.h
